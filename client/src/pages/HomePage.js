bd0b53d1dca4565fea0806ee0fea6057604d11b1
futoidaddy <57603310+hametar0u@users.noreply.github.com>
2022-04-29 14:20:09 -0700

    code verifier not working

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,28 +1,27 @@
-import { useEffect } from "react";
+import { useContext } from "react";
+import { UserContext } from "../App";
+import axios from 'axios';
 
 const HomePage = (props) => {
-  // useEffect(
-  //   () => {
-  //     const getAccessToken = async () => {
-  //       const params = {
-  //         code: props.code,
-  //         state: props.state
-  //       };
-        
-  //       const response = await axios.post("/auth/token", params);
-  //       const json = await response.data;
-  //       console.log(json);
-  //     }
-
-  //     await getAccessToken();
-  //   }
-  // );
-
+  const userData = useContext(UserContext);
+  const getAccessToken = async () => {
+    const params = {
+      code: props.code,
+      state: props.state,
+      code_verifier: userData.code_challenge
+    };
+    
+    const response = await axios.post("/auth/token", params);
+    const json = await response.data;
+    console.log(json);
+  }
   return(
     <div>
       hi
-      <p>{props.code}</p>
-      <p>{props.state}</p>
+      <p>code: {props.code}</p>
+      <p>state: {props.state}</p>
+      <p>code challenge: {userData.code_challenge}</p>
+      <button onClick={getAccessToken}>get access token</button>
     </div>
   );
 }
--- server/routes/MAL.js
+++ server/routes/MAL.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const axios = require('axios');
+const querystring = require('querystring');
 
 const MALRoutes = express.Router();
 
 MALRoutes.route("/auth").post(async (req, res) => {
-  const code_challenge = req.body.code_challenge;
-  const url = `https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&code_challenge=${code_challenge}&state=RequestID42`;
+  const code_challenge = req.body.code_challenge.trim();
+  const url = `https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&code_challenge=${code_challenge}&code_challenge_method=plain&state=RequestID42`;
   res.send({ url });
 });
 
 MALRoutes.route("/auth/token").post(async (req, res) => {
-  const code = req.body.code;
-  // const url = `https://myanimelist.net/v1/oauth2/token?client_id=${process.env.CLIENT_ID}?client_secret=${process.env.CLIENT_SECRET}?code=${code}?code_verifier=${}`;
+  console.log("code_verifier:",req.body.code_verifier);
+  const params = querystring.stringify({
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    grant_type: "authorization_code",
+    code: req.body.code,
+    code_verifier: req.body.code_verifier.trim(),
+    
+  });
+
+  const url = `https://myanimelist.net/v2/oauth2/token`;
+  const response = await axios.post(url, params);
+  const json = await response.data;
+  // console.log(json);
+  res.send(json);
 });
 
 module.exports = MALRoutes;
