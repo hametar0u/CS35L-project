c79b7a35a7d56e2ecbf6bd975c1f61b31a102b91
margaretcapetz <pcdcmccc@gmail.com>
2022-05-07 11:22:59 -0700

    add and del animes from list

--- client/src/testpages/UserTestTest.js
+++ client/src/testpages/UserTestTest.js
@@ -19,9 +19,47 @@ const UserTestTest = (props) => {
     });
   };
 
+  const deleteFromList = async () => {
+      console.log("i got to del")
+    await axios.delete(`/deleteFromList/28977`, { //replace the id
+      withCredentials: true,
+
+      data: {
+        id: 28977
+      }
+    })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+      setError(err.response);
+    });
+  };
+
+  const addToList = async () => {
+    console.log("i got to post")
+  await axios.post(`/addToList/28977`, { //replace the id
+    withCredentials: true,
+
+    data: {
+      id: 28977
+    }
+  })
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+    setError(err.response);
+  });
+};
+
   return (
     <>
       <button onClick={getUserTest}>get user list</button>
+      <button onClick={deleteFromList}>delete from list</button>
+      <button onClick={addToList}>add to list</button>
     </>
   );
 };
--- server/routes/UserTest.js
+++ server/routes/UserTest.js
@@ -33,13 +33,70 @@ UserTest.route("/usertest").get(async (req, res) => {
 
 });
 
+UserTest.route("/deleteFromList/:id").delete(async (req, res) => {
+    console.log("i got to del");
+    console.log(req.session);
+  
+    const access_token = req.session.tokens.access_token;
+    const url = `https://api.myanimelist.net/v2/anime/${req.body.id}/my_list_status`;
+    const config = {
+        headers: {
+          Authorization: "Bearer " + access_token,
+        },
+        
+    };
+  
+      await axios
+        .delete(url, config)
+        .then((response) => {
+          console.log(response.data);
+          res.status(200).send(response.data);
+        })
+        .catch((err) => {
+          //TODO: if access token expired, redirect to /auth/refresh-token
+          console.log(err.response);
+          if (err.response.status === 401) {
+              res.redirect(307, "/auth/v2"); //TODO: TEMPORARY
+          }
+          next(err);
+        });
+  
+  });
 
-// const getUserList = async () => {
-//     const params = userAuth;
-//     const response = await axios.post("/get-user-list", params);
-//     const json = await response.data;
-//     console.log(json);
-//     // setUserList(json.name);
-//   };
+
+  UserTest.route("/addToList/:id").post(async (req, res) => {
+    console.log("i got to post");
+    console.log(req.session);
+
+    // if(!req.session.tokens) res.redirect(307,"/auth/v2/login");
+    // else {
+        
+    // }
+    const access_token = req.session.tokens.access_token;
+    const url = `https://api.myanimelist.net/v2/anime/${req.body.id}/my_list_status`;
+    const config = {
+        headers: {
+          Authorization: "Bearer " + access_token,
+        },
+        
+    };
+  
+      await axios
+        .post(url, config)
+        .then((response) => {
+          console.log(response.data);
+          res.status(200).send(response.data);
+        })
+        .catch((err) => {
+          //TODO: if access token expired, redirect to /auth/refresh-token
+          console.log(err.response);
+          if (err.response.status === 401) {
+              res.redirect(307, "/auth/v2"); //TODO: TEMPORARY
+          }
+          next(err);
+        });
+  
+  });
+
 
 module.exports = UserTest;
