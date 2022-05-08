f7ed8374eeac0eb6ed69366ad367e83f4dfc7524
futoidaddy <57603310+hametar0u@users.noreply.github.com>
2022-05-08 14:10:34 -0700

    integration

--- client/src/testpages/UserTestTest.js
+++ client/src/testpages/UserTestTest.js
@@ -1,10 +1,24 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 const UserTestTest = (props) => {
+  const input = useRef();
   const [error, setError] = useState();
   const [userList, setUserList] = useState();
 
+  const checkSession = async () => {
+    await axios
+      .get("/checksession", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(err.response);
+      });
+  };
 
   const getUserTest = async () => {
     await axios.get("/usertest", {
@@ -19,9 +33,53 @@ const UserTestTest = (props) => {
     });
   };
 
+  const deleteFromList = async () => {
+      console.log("i got to del")
+
+    const params = {
+      id: input.current.value
+    };
+
+    await axios.delete(`/deleteFromList/${input.current.value}`, {
+      withCredentials: true,
+      data: params
+    })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+      setError(err.response);
+    });
+  };
+  
+
+  const addToList = async () => {
+    console.log("i got to patch")
+  
+  const config = {
+    withCredentials: true
+  };
+  const params = {
+    id: input.current.value
+  };
+  await axios.post(`/addToList/${input.current.value}`, params, config)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+    setError(err.response);
+  });
+};
+
   return (
     <>
       <button onClick={getUserTest}>get user list</button>
+      <input ref={input} placeholder="type anime id" />
+      <button onClick={deleteFromList}>delete from list</button>
+      <button onClick={addToList}>add to list</button>
+      <button onClick={checkSession}>check whats in session</button>
     </>
   );
 };
--- server/routes/UserTest.js
+++ server/routes/UserTest.js
@@ -33,13 +33,102 @@ UserTest.route("/usertest").get(async (req, res) => {
 
 });
 
+UserTest.route("/deleteFromList/:id").delete(async (req, res) => {
+    console.log("i got to del");
+    console.log(req.session);
+  
+    const access_token = req.session.tokens.access_token;
+    const url = `https://api.myanimelist.net/v2/anime/${req.body.id}/my_list_status`;
+    const config = {
+        headers: {
+          Authorization: "Bearer " + access_token,
+        },
+        
+    };
+  
+      await axios
+        .delete(url, config)
+        .then((response) => {
+          console.log(response.data);
+          res.status(200).send(response.data);
+        })
+        .catch((err) => {
+          //TODO: if access token expired, redirect to /auth/refresh-token
+          console.log(err.response);
+          if (err.response.status === 401) {
+              res.redirect(307, "/auth/v2"); //TODO: TEMPORARY
+          }
+          next(err);
+        });
+  
+  });
 
-// const getUserList = async () => {
-//     const params = userAuth;
-//     const response = await axios.post("/get-user-list", params);
-//     const json = await response.data;
-//     console.log(json);
-//     // setUserList(json.name);
-//   };
+
+//   UserTest.route("/addToList/:id").patch(async (req, res, next) => {
+//     console.log("i got to patch");
+//     console.log(req.session);
+
+//     if(!req.session.tokens) res.send("no tokens");
+//     else {
+//       const access_token = req.session.tokens.access_token;
+//       const url = `https://api.myanimelist.net/v2/anime/${req.body.id}/my_list_status`;
+//       const config = {
+//           headers: {
+//             Authorization: "Bearer " + access_token,
+//           },
+          
+//       };
+    
+//       await axios
+//         .patch(url, config)
+//         .then((response) => {
+//           console.log(response.data);
+//           res.status(200).send(response.data);
+//         })
+//         .catch((err) => {
+//           //TODO: if access token expired, redirect to /auth/refresh-token
+//           console.log(err.response);
+//           if (err.response.status === 401) {
+//               res.redirect(307, "/auth/v2"); //TODO: TEMPORARY
+//           }
+//           next(err);
+//         });
+//     }
+  
+//   });
+
+UserTest.route("/addToList/:id").post(async (req, res, next) => {
+    console.log("i got to patch");
+    console.log(req.session);
+
+    if(!req.session.tokens) res.send("no tokens");
+    else {
+      const access_token = req.session.tokens.access_token;
+      const url = `https://api.myanimelist.net/v2/anime/${req.body.id}/my_list_status`;
+      const config = {
+          headers: {
+            Authorization: "Bearer " + access_token,
+          },
+          
+      };
+
+    
+      await axios
+        .patch(url, {}, config)
+        .then((response) => {
+          console.log(response.data);
+          res.status(200).send(response.data);
+        })
+        .catch((err) => {
+          //TODO: if access token expired, redirect to /auth/refresh-token
+          console.log(err.response);
+          if (err.response.status === 401) {
+              res.redirect(307, "/auth/v2"); //TODO: TEMPORARY
+          }
+          next(err); //NOTE: this will return a 500 if the anime you try to add is already in the list
+        });
+    }
+  
+  });
 
 module.exports = UserTest;
