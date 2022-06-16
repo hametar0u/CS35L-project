a5e48fc3fdad56e0471c3ce86d4627dc7278f51f
hametar0u <57603310+hametar0u@users.noreply.github.com>
2022-06-16 07:53:55 -0700

    try alternate pathname resolution
    heroku for some reason goes into /app/client/build/index.html

--- server.js
+++ server.js
@@ -57,10 +57,10 @@ app.use(function (err, req, res, next) {
   next(err);
 });
 
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 app.listen(port, () => {
